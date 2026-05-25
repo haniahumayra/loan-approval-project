0bb59568875a7f2c400405587b9bfddeae9497dc
Haniah Humayra <haniahmyr@gmail.com>
2026-05-25 11:58:32 +0700

    Update Homepage

--- app.py
+++ app.py
@@ -25,10 +25,6 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
 
 @app.route('/predict', methods=['POST'])
 def predict():
